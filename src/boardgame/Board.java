ec2c7123f507adf726da824a50cf34bf4af9445b
João Marques <joaomarques.jm991@gmail.com>
2021-04-08 23:31:44 -0300

    Final project

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,40 +1,37 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
 	
-	
 	public Board(int rows, int columns) {
-		if(rows < 1 || columns < 1) {
+		if (rows < 1 || columns < 1) {
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
-		pieces = new Piece [rows][columns];
+		pieces = new Piece[rows][columns];
 	}
-
 
 	public int getRows() {
 		return rows;
 	}
 
-
 	public int getColumns() {
 		return columns;
 	}
 	
-	public Piece piece (int row, int column) {
-		if(!positionExists(row, column)) {
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
-		if(!positionExists(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
@@ -48,6 +45,19 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -57,10 +67,9 @@ public class Board {
 	}
 	
 	public boolean thereIsAPiece(Position position) {
-		if(!positionExists(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return piece(position) != null;
 	}
-	
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,31 +1,27 @@
+  
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
-	
 	
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
 
-
 	public int getRow() {
 		return row;
 	}
-
 
 	public void setRow(int row) {
 		this.row = row;
 	}
 
-
 	public int getColumn() {
 		return column;
 	}
-
 
 	public void setColumn(int column) {
 		this.column = column;
@@ -36,10 +32,8 @@ public class Position {
 		this.column = column;
 	}
 	
-	
 	@Override
 	public String toString() {
-		return  row + ", " + column;
+		return row + ", " + column;
 	}
-
 }
